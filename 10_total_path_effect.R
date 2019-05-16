46d1496df5ffb5ba7c7c542e883fbd85f638487c
anneMehl <anne.mehlhoop@nina.no>
2019-05-16 15:35:23 +0200

    -

--- 10_total_path_effect.R
+++ 10_total_path_effect.R
@@ -31,9 +31,9 @@ source("function_resp_curve.R")
 
 
 (focal = c("distvei", "skogkategori", "treartgruppe", "kant", 
-           "helling", "HOH", "tretetthet", "browspres_merged")[1])
-h2_distvei <- resp_curve(focal, dat1, dat2, zeromod1, percmod1, zeromod2, percmod2, 
-           which_effect=c("hurdle1_only", "hurdle2_only", "total")[2], nvals=100, plotit=F, nb_btstrp=99)
+           "helling", "HOH", "tretetthet", "browspres_merged")[2])
+t_treart <- resp_curve(focal, dat1, dat2, zeromod1, percmod1, zeromod2, percmod2, 
+           which_effect=c("hurdle1_only", "hurdle2_only", "total")[3], nvals=100, plotit=T, nb_btstrp=99)
 
 #for debugging only
 #nvals=10
@@ -43,7 +43,7 @@ h2_distvei <- resp_curve(focal, dat1, dat2, zeromod1, percmod1, zeromod2, percmo
 
 
 
-# h1_distvei$var <- exp(h1_distvei$var)-1
+h1_distvei$var <- exp(h1_distvei$var)-1
 # h1_hoh$var <- h1_hoh$var * 244.7748 + 361.8423
 
 h2_distvei$var <- exp(h2_distvei$var)-1
@@ -52,36 +52,36 @@ h2_distvei$var <- exp(h2_distvei$var)-1
 
 library(ggplot2)
 
-# ggplot(data = h1_distvei, aes(var, point)) +
-#   geom_line(colour = "#009292", linetype = 1, size = 1)+
-#   geom_ribbon(aes(ymin = CI025, ymax = CI975), colour = "#004949", linetype = 2, alpha = 0.2) +
-#   theme_light() +
-#   theme(axis.text = element_text(size = 10),
-#         axis.title = element_text(family = "sans", size = 14, color = "grey30")) +
-#   xlab("\nDistance to road (m)") + 
-#   ylab("Browsing pressure (%)\n") 
+ggplot(data = h1_distvei, aes(var, point)) +
+  geom_line(colour = "#009292", linetype = 1, size = 1)+
+  geom_ribbon(aes(ymin = CI025, ymax = CI975), colour = "#004949", linetype = 2, alpha = 0.2) +
+  theme_light() +
+  theme(axis.text = element_text(size = 10),
+        axis.title = element_text(family = "sans", size = 14, color = "grey30")) +
+  xlab("\nDistance to road (m)") +
+  ylab("Browsing pressure (%)\n")
 # 
 # 
-# ggplot(data = h1_skogkat,aes(var, point)) +
-#   geom_pointrange(aes(ymin = CI025, ymax = CI975), colour = "#009292", size = 1) +
-#   theme_light() +
-#   theme(axis.text = element_text(size = 10),
-#         axis.title = element_text(family = "sans", size = 14, color = "grey30")) +
-#   xlab("\nForest category") + 
-#   ylab("Browsing pressure (%)\n") +
-#   scale_x_discrete(labels = c("Highproductive \nold forest", "Highproductive \nyoung forest", 
-#                               "Lowproductive \nold forest", "Lowproductive \nyoung forest", 
-#                               "Unproductive \nforest"))
-# 
-# 
-# ggplot(data = h1_treart,aes(var, point)) +
-#   geom_pointrange(aes(ymin = CI025, ymax = CI975), colour = "#009292", size = 1) +
-#   theme_light() +
-#   theme(axis.text = element_text(size = 10),
-#         axis.title = element_text(family = "sans", size = 14, color = "grey30")) +
-#   xlab("\nTree species groups") + 
-#   ylab("Browsing pressure (%)\n") +
-#   scale_x_discrete(labels = c("Pine", "Other deciduous \ntrees", "Rowan, aspen, \nwillow"))
+ggplot(data = h1_skogkat,aes(var, point)) +
+  geom_pointrange(aes(ymin = CI025, ymax = CI975), colour = "#009292", size = 1) +
+  theme_light() +
+  theme(axis.text = element_text(size = 10),
+        axis.title = element_text(family = "sans", size = 14, color = "grey30")) +
+  xlab("\nForest category") +
+  ylab("Browsing pressure (%)\n") +
+  scale_x_discrete(labels = c("Highproductive \nold forest", "Highproductive \nyoung forest",
+                              "Lowproductive \nold forest", "Lowproductive \nyoung forest",
+                              "Unproductive \nforest"))
+
+
+ggplot(data = h1_treart,aes(var, point)) +
+  geom_pointrange(aes(ymin = CI025, ymax = CI975), colour = "#009292", size = 1) +
+  theme_light() +
+  theme(axis.text = element_text(size = 10),
+        axis.title = element_text(family = "sans", size = 14, color = "grey30")) +
+  xlab("\nTree species groups") +
+  ylab("Browsing pressure (%)\n") +
+  scale_x_discrete(labels = c("Pine", "Other deciduous \ntrees", "Rowan, aspen, \nwillow"))
 # 
 # 
 # ggplot(data = h1_kant, aes(var, point)) +
